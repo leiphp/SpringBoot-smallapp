1d5ce0ec36370ea102e2fa88a2e9c7e17fdb3130
leiwen <657106593@qq.com>
2019-06-01 00:00:43 +0800

    feat(servie):新增service和impl实现

---
+++ src/main/java/cn/lxtkj/demo/config/service/TransactionManagementConfiguration.java
@@ -0,0 +1,20 @@
+package cn.lxtkj.demo.config.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionManagementConfiguration implements TransactionManagementConfigurer {
+    private DataSource dataSource;
+
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
---
+++ src/main/java/cn/lxtkj/demo/service/AreaService.java
@@ -0,0 +1,41 @@
+package cn.lxtkj.demo.service;
+
+import cn.lxtkj.demo.entity.Area;
+
+import java.util.List;
+
+public interface AreaService {
+    /**
+     * 获取区域列表
+     * @return
+     */
+    List<Area> getAreaList();
+
+    /**
+     * 根据Id列出具体区域
+     * @param areaId
+     * @return
+     */
+    Area getAreaById(int areaId);
+
+    /**
+     * 增加区域信息
+     * @param area
+     * @return
+     */
+    boolean addArea(Area area);
+
+    /**
+     * 修改区域信息
+     * @param area
+     * @return
+     */
+    boolean modifyArea(Area area);
+
+    /**
+     * 删除区域信息
+     * @param areaId
+     * @return
+     */
+    boolean deleteArea(int areaId);
+}
